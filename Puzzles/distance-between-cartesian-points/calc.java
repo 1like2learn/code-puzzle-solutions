d20d199c484a175c446c9fe256152d75ec8cef81
1like2learn <ndespained@gmail.com>
2021-02-12 11:21:10 -0800

    distance between cartesian points java

---
+++ Puzzles/distance-between-cartesian-points/calc.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// ArrayList<ArrayList<Integer>> data = [[0,0],[0,1],[1,1],[1,0]]
+// ArrayList<ArrayList<Integer>> data = [[1,1], [1,2], [2,2], [2,1], [1,1]]
+
+class distanceBetweenCartesianPoints {
+    public double calc(ArrayList<ArrayList<Integer>> points) {
+
+        return 0;
+    }
+}
+public static void main(String args[]){
+    distanceBetweenCartesianPoints calc = new distanceBetweenCartesianPoints();
+
+    int[][] points = {}
+ 
+    calc.calc(points);
+}
